1c373526948b869c30913c006bc6ec9590542941
VieEeEw <786851669@qq.com>
2020-11-16 23:16:04 -0600

    Minor change

--- gan/models.py
+++ gan/models.py
@@ -27,19 +27,19 @@ class Generator(torch.nn.Module):
     def __init__(self, noise_dim, out_ch=3):
         super(Generator, self).__init__()
         self.model = nn.Sequential(
-            nn.ConvTranspose2d(noise_dim, 1024, 4, padding=0),
+            nn.ConvTranspose2d(noise_dim, 1024, 4),
             nn.BatchNorm2d(1024),
             nn.ReLU(),
-            nn.ConvTranspose2d(1024, 512, 4, 2),
+            nn.ConvTranspose2d(1024, 512, 4, 2, 1),
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.ConvTranspose2d(512, 256, 4, 2),
+            nn.ConvTranspose2d(512, 256, 4, 2, 1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.ConvTranspose2d(256, 128, 4, 2),
+            nn.ConvTranspose2d(256, 128, 4, 2, 1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.ConvTranspose2d(128, out_ch, 4, 2),
+            nn.ConvTranspose2d(128, out_ch, 4, 2, 1),
             nn.Tanh()
         )
 
